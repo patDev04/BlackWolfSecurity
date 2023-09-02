a95944be9d4316b6b6ed6fb345378d32fde1af92
patrick lema <patrick_lema_dev@hotmail.com>
2023-09-02 14:16:16 +0100

    third-commit

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,7 @@ document.body.insertAdjacentHTML(
     <nav>
         <main>
             <div class='nav'>
-                <a href='./index.html'><img src="../images/lOGO.svg" alt="logo" class="nav-logo"/></a>
+                <a href='./index.html'><img src="./images/LOGO.svg" alt="footer-logo" class="nav-logo" /></a>
                 <div class="nav-item">
                     <button><a href="./index.html">Home</a></button>
                     <button class="nav-link"><a href="./about.html">About</a></button>
