7c79d1fc80ff4bf2c2690e4eaf4e6fd9554fe7df
patrick lema <patrick_lema_dev@hotmail.com>
2023-09-04 19:13:22 +0100

    slogan change

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,10 @@ document.body.insertAdjacentHTML(
     <nav>
         <main>
             <div class='nav'>
+            <div class="logo-container">
                 <a href='./index.html'><img src="./images/LOGO.svg" alt="footer-logo" class="nav-logo" /></a>
+                <p class="footer-slogan">Canine Vigilance </br> Human Assurance</p>
+            </div>
                 <div class="nav-item">
                     <button><a href="./index.html">Home</a></button>
                     <button class="nav-link"><a href="./about.html">About</a></button>
