9a07d12bdece9ceda3cf2bd8729881e7b462a885
patrick lema <patrick_lema_dev@hotmail.com>
2023-09-04 19:36:14 +0100

    slogan removal

--- js/navbar.js
+++ js/navbar.js
@@ -8,7 +8,6 @@ document.body.insertAdjacentHTML(
             <div class='nav'>
             <div class="logo-container">
                 <a href='./index.html'><img src="./images/LOGO.svg" alt="footer-logo" class="nav-logo" /></a>
-                <p class="footer-slogan">Canine Vigilance </br> Human Assurance</p>
             </div>
                 <div class="nav-item">
                     <button><a href="./index.html">Home</a></button>
